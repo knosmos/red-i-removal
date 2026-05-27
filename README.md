060261567f4233663e84e50c6b519555b4ff1f54
Jieruei Chang <knosmos@gmail.com>
2026-05-26 21:32:48 -0400

    feat: more visualizations

--- README.md
+++ README.md
@@ -3,3 +3,7 @@ Solves the longstanding problem of red i removal (pain of photographers everywhe
 characters. Optimization is standard Adam + MSE loss.
 
 ![Model Visualization](outputs/visualization.png)
+
+## Usage
+- `train.py` - trains the model on the synthetic dataset and saves the best model to `outputs/best_model.pth`. Expects a dataset .npz file `train_data_batch_1`.
+- `test.py` - loads the best model and visualizes the results on a few samples from the dataset. Saves the visualization to `outputs/visualization.png`.
--- test.py
+++ test.py
@@ -11,17 +11,11 @@ def visualize_samples(inputs, targets, outputs):
         len(inputs) == len(targets) == len(outputs)
     ), "Inputs, targets, and outputs must have the same length."
     num_samples = len(inputs)
-    plt.figure(figsize=(9, 3 * num_samples))
-    # plt.subplot(1, 3, 1)
-    # plt.title("Input")
-    # plt.subplot(1, 3, 2)
-    # plt.title("Ground Truth")
-    # plt.subplot(1, 3, 3)
-    # plt.title("Output")
+    plt.figure(figsize=(3 * num_samples, 9))
     for idx, (sample_input, sample_gt, sample_output) in enumerate(
         zip(inputs, targets, outputs)
     ):
-        plt.subplot(num_samples, 3, idx * 3 + 1)
+        plt.subplot(3, num_samples, 0 * num_samples + idx + 1)
         plt.imshow(
             (sample_input.squeeze().permute(1, 2, 0) * 255)
             .cpu()
@@ -29,14 +23,14 @@ def visualize_samples(inputs, targets, outputs):
             .astype(np.uint8)
         )
         if idx == 0:
-            plt.title("Input")
-        plt.subplot(num_samples, 3, idx * 3 + 2)
+            plt.ylabel("Input", rotation=0, labelpad=40)
+        plt.subplot(3, num_samples, 1 * num_samples + idx + 1)
         plt.imshow(
             (sample_gt.squeeze().permute(1, 2, 0) * 255).numpy().astype(np.uint8)
         )
         if idx == 0:
-            plt.title("Ground Truth")
-        plt.subplot(num_samples, 3, idx * 3 + 3)
+            plt.ylabel("Ground Truth", rotation=0, labelpad=40)
+        plt.subplot(3, num_samples, 2 * num_samples + idx + 1)
         plt.imshow(
             (sample_output.squeeze().detach().permute(1, 2, 0) * 255)
             .cpu()
@@ -44,7 +38,7 @@ def visualize_samples(inputs, targets, outputs):
             .astype(np.uint8)
         )
         if idx == 0:
-            plt.title("Output")
+            plt.ylabel("Output", rotation=0, labelpad=40)
     plt.tight_layout()
     plt.savefig("outputs/visualization.png")
     plt.show()
@@ -55,7 +49,7 @@ if __name__ == "__main__":
     model = RedIRemover()
     model.load_state_dict(torch.load("outputs/best_model.pth"))
     inputs, targets, outputs = [], [], []
-    for i in range(10, 14):
+    for i in range(10, 20):
         sample = dataset[i]
         sample_input = sample["input"].unsqueeze(0)
         sample_gt = sample["gt"].unsqueeze(0)
